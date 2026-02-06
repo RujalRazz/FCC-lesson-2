fa6f8d4986eadc5f9d0018928464b00a7636aa2f
Rujal Sharma Rajopadhyaya <rujalsharma17@gmail.com>
2026-02-06 13:32:59 +0545

    Lessons

--- myApp.js
+++ myApp.js
@@ -16,10 +16,14 @@ app.get('/json', (req, res) => {
     }
   res.json({"message": message})
 })
-
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next()
+})  
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
 
  module.exports = app;
+console.log(process.env.MESSAGE_STYLE);
