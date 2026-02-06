9593c3f337995932e7840d88883bb8f4901335f0
Rujal Sharma Rajopadhyaya <rujalsharma17@gmail.com>
2026-02-06 13:20:06 +0545

    Lessons

--- myApp.js
+++ myApp.js
@@ -1,11 +1,25 @@
 let express = require('express');
 let app = express();
+require('dotenv').config()
+const port = 3000
+
+app.use("/public", express.static(__dirname + "/public"))
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + "/views/index.html")
 })
+app.get('/json', (req, res) => {
+    let message = "Hello json"
 
+    if(process.env.MESSAGE_STYLE === "uppercase"){
+        message = message.toUpperCase();
+    }
+  res.json({"message": message})
+})
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 
 
  module.exports = app;
